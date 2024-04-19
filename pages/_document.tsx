2994278493d741ee7ba13e85ca848c9bc58d2cae
서준형 <junhyeong3289@gmail.com>
2024-04-19 11:06:58 +0900

    feat: google-site-verification 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-          <title>퍼펙트솔루션 ㅣ카지노솔루션 ㅣ API, 임대, 분양, 제작</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <meta name="google-site-verification" content="R-x2EYr4ZV0015ouebBNv5ZAVGLGYImdh3f_9hX4vbg"/>
+            <title>퍼펙트솔루션 ㅣ카지노솔루션 ㅣ API, 임대, 분양, 제작</title>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
