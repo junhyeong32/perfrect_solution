94e5a0302d2d2b78503b872ae136321873a90340
서준형 <junhyeong3289@gmail.com>
2024-05-03 20:58:25 +0900

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
             <Head>
                 <meta name="google-site-verification" content="R-x2EYr4ZV0015ouebBNv5ZAVGLGYImdh3f_9hX4vbg"/>
                 <title>퍼펙트솔루션 ㅣ카지노솔루션 ㅣ API, 임대, 분양, 제작</title>
+                <link rel="icon" type="image/svg+xml" href="/icon.jpeg"/>
+                <meta property="og:title" content="퍼펙트솔루션"/>
+                <meta property="og:type" content="website"/>
                 <meta name="Description"
                       content="퍼펙트솔루션, 카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>
                 <meta name="Keywords"
