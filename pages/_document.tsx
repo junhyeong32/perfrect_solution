105598c5dc0a55cd5e577b359579af30dc0880d6
서준형 <junhyeong3289@gmail.com>
2024-05-03 12:11:28 +0900

    feat: add meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Html, Head, Main, NextScript} from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-        <Head>
-            <meta name="google-site-verification" content="R-x2EYr4ZV0015ouebBNv5ZAVGLGYImdh3f_9hX4vbg"/>
-            <title>퍼펙트솔루션 ㅣ카지노솔루션 ㅣ API, 임대, 분양, 제작</title>
-        </Head>
-        <body>
-        <Main/>
-        <NextScript/>
-        </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="google-site-verification" content="R-x2EYr4ZV0015ouebBNv5ZAVGLGYImdh3f_9hX4vbg"/>
+                <title>퍼펙트솔루션 ㅣ카지노솔루션 ㅣ API, 임대, 분양, 제작</title>
+                <meta name="Description"
+                      content="퍼펙트솔루션,카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>
+                <meta name="Keywords"
+                      content="퍼펙트솔루션,카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>/>
+                <meta name="keywords"
+                      content="퍼펙트솔루션,카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>/>
+                <meta name="description"
+                      content="퍼펙트솔루션,카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>/>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
 }
