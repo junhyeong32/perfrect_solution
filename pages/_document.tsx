9a1e9e69f5b680452aef92cb59bf90cf658d8406
서준형 <junhyeong3289@gmail.com>
2024-05-26 17:38:53 +0900

    feat: add meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,13 +10,19 @@ export default function Document() {
                 <meta property="og:title" content="퍼펙트솔루션"/>
                 <meta property="og:type" content="website"/>
                 <meta name="Description"
-                      content="퍼펙트솔루션, 카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>
+                      content="카지노분양,카지노제작,토지노분양,카지노솔루션,사이트제작"/>
                 <meta name="Keywords"
                       content="퍼펙트솔루션, 카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>
                 <meta name="keywords"
                       content="퍼펙트솔루션, 카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>
                 <meta name="description"
-                      content="퍼펙트솔루션, 카지노솔루션, 토지노솔루션, 홀덤솔루션, 토토솔루션, 알api, 사이트제작, 솔루션분양, 솔루션제작"/>
+                      content="카지노분양, 카지노제작, 토지노분양, 카지노솔루션, 사이트제작"/>
+                <meta property="og:image" content="/icon.jpeg" />
+                <meta property="og:url" content="pf-sol.net" />
+                <meta property="twitter:card" content="카지노분양,카지노제작,토지노분양,카지노솔루션,사이트제작" />
+                <meta property="twitter:title" content="카지노솔루션 업계 1위 퍼펙트 솔루션" />
+                <meta property="twitter:description" content="카지노분양,카지노제작,토지노분양,카지노솔루션,사이트제작" />
+                <meta property="twitter:image" content="/icon.jpeg" />
             </Head>
             <body>
             <Main/>
